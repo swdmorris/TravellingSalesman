4822f2c187a7da765ca676635b5ebc0fddcc5d5c
Spencer Morris <spencer@ticketscalpr.com>
2015-01-22 19:54:12 -0600

    Initial Commit

---
+++ TravellingSalesman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravellingSalesman
+//
+//  Created by Spencer Morris on 1/22/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravellingSalesman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TravellingSalesman
+//
+//  Created by Spencer Morris on 1/22/15.
+//  Copyright (c) 2015 SpencerMorris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
